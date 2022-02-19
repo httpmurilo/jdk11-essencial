0a0dc48b5bf87c6c2f9c464babc33ed24f0004dd
httpmurilo <muriloeduardo1997@outlook.com>
2022-02-19 18:35:59 -0300

    armazenamento sequencial

--- src/io/murilo/estruturadedados/ArmazenamentoSequencial/Programa.java
+++ src/io/murilo/estruturadedados/ArmazenamentoSequencial/Programa.java
@@ -2,11 +2,27 @@ package io.murilo.estruturadedados.ArmazenamentoSequencial;
 
 public class Programa {
     public static void main(String[] args) {
-        int[] numeros =  new int[5];
+        var a1 = new Aluno("Murilo");
+        var a2 = new Aluno("Matheus");
 
-        for(int i=0; i < numeros.length ; i++ ){
-            System.out.println(numeros[i]);
-        }
+        Vetor lista = new Vetor();
+
+        System.out.println("Totl de aluno: " + lista.tamanho());
+        lista.adicionarAluno(a1);
+        lista.adicionarAluno(a2);
+
+        System.out.println("Totl de aluno: " + lista.tamanho());
+
+        System.out.println(lista);
+        System.out.println(lista.existeAluno(a1));
+        System.out.println(lista.existeAluno(a2));
+        System.out.println(lista.pegarPosicao(0));
+        System.out.println(lista.pegarPosicao(1));
+
+        var a3 = new Aluno("Murilo Ed");
+        lista.adicionarAluno(1, a3);
+        System.out.println(lista);
+
 
     }
 }
--- src/io/murilo/estruturadedados/ArmazenamentoSequencial/Vetor.java
+++ src/io/murilo/estruturadedados/ArmazenamentoSequencial/Vetor.java
@@ -5,24 +5,57 @@ import java.util.Arrays;
 public class Vetor {
     private Aluno alunos[] = new Aluno[100];
 
+    private  Integer TOTAL = 0;
+
+    public void adicionarAluno(int posicao, Aluno aluno) {
+        //adiciona o aluno na posicao, o aluno que ja está na posicao sobe uma posicao
+        for(int i = TOTAL -1; i >= posicao; i-=1) {
+            alunos[i+1] = alunos[i];
+        }
+        alunos[posicao] = aluno;
+
+    }
+
     public void adicionarAluno(Aluno aluno) {
-        //TODO receber um aluno
+//        for (int i=0; i< alunos.length; i++) {
+//            if(alunos[i] == null) {
+//                alunos[i] = aluno;
+//                break;
+//            }
+//        }
+        this.alunos[TOTAL] = aluno;
+        TOTAL = TOTAL + 1;
     }
 
     public Aluno pegarPosicao(int posicao) {
+        if(!posicaoOcupada(posicao)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
         return this.alunos[posicao];
     }
 
+    private boolean posicaoOcupada(int posicao) {
+        return posicao >= 0 && posicao < TOTAL;
+    }
+
     public void removerAluno(int posicao) {
-        this.alunos[posicao] = null;
+        for(int i = posicao; i< this.TOTAL; i++) {
+            this.alunos[i] = this.alunos[i+1];
+        }
+        TOTAL --;
     }
 
     public boolean existeAluno(Aluno aluno) {
+        for (Aluno value : alunos) {
+            if (aluno.equals(value)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public int tamanho() {
-        return 0;
+        return TOTAL;
     }
 
     @Override
